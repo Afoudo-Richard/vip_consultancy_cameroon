a705e099cdba2658ea4baa1c40c100a0f31ad2c7
Richard Afoudo <richardafoudo07@gmail.com>
2022-05-05 09:57:13 +0100

    Added email functionality and tested

--- vip_consulting_web/frontend/views.py
+++ vip_consulting_web/frontend/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
+from django.core.mail import send_mail,EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def login(request):
     return render(request, 'login.html')
 
+
 def book_consultation(request):
+    msg_html = render_to_string('custom-email.html', {'name': "Richard Arc"})
+    # send_mail(
+    #     subject='Hello from ArcTest on VIP consultanc cameroon',
+    #     message='A stunning message',
+    #     from_email=settings.EMAIL_HOST_USER,
+    #     recipient_list=['richardafoudo07@gmail.com'],
+    #     html_message=msg_html,
+    #     )
+
+    email = EmailMessage("Test from my Arc", msg_html, settings.EMAIL_HOST_USER, ['richardafoudo07@gmail.com'])
+    email.fail_silently = True
+    email.content_subtype = "html"
+    #email.send()
     return render(request, 'book-consultation.html')
--- vip_consulting_web/vip_consulting_web/settings.py
+++ vip_consulting_web/vip_consulting_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,10 +134,16 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+env = environ.Env()
+environ.Env.read_env()
+
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
+# Custom setting. To email
+RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
